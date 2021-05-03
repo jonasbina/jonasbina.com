2f87b98202fc36bd83bc0fed8c673a7334cda190
Jonas <jonysbina@gmail.com>
2021-05-03 10:45:02 +0200

    InitCommit

---
+++ pages/aboutme.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+    <h4>My name is Jonas Bina</h4>
+    <a>I live in the czech republic</a>
+    <a>I programming android apps</a>
+    </div>
+  )
+}
