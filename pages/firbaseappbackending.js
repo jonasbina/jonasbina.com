236ee2fa7588ccb3a8804c4cd7dbed4626985d15
Jonas <jonysbina@gmail.com>
2022-02-06 14:11:30 +0100

    Hey

--- pages/firbaseappbackending.js
+++ pages/firbaseappbackending.js
@@ -1,12 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {database} from "firebase/database"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// import {database} from "firebase/database"
 const firebaseConfig = {
   apiKey: "AIzaSyDkHlY6WhmCydfBGRUssKKFMui6a_deNvc",
   authDomain: "newcoolapp-87f58.firebaseapp.com",
@@ -21,4 +16,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-database.reference("l").child("xd").child("lel").setValue("xd")
+// database.reference("l").child("xd").child("lel").setValue("xd")
