1a8b9c752d47ccfde63a7b11b6d9707b70f432b7
Jonas <jonysbina@gmail.com>
2022-02-06 15:44:58 +0100

    Yay

--- firebase/initFirebase.js
+++ firebase/initFirebase.js
@@ -13,7 +13,7 @@ import { getDatabase } from "@firebase/database";
 const firebaseConfig = {
     apiKey: process.env.NEXT_API_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_API_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.NEXT_API_FIREBASE_DATABASE_URL,
+    databaseURL: process.env.NEXT_API_FIREBASE_DATA_URL,
     projectId: process.env.NEXT_API_FIREBASE_PROJECT_ID,
     storageBucket: process.env.NEXT_API_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_API_FIREBASE_MESSAGING_SENDER_ID,
