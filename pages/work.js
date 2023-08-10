e0f405e55daef4dbf81b5462f9317e878035e525
Jonas <jonysbina@gmail.com>
2023-08-10 21:48:58 +0200

    few edits to my grammar

--- pages/work.js
+++ pages/work.js
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-    <h1>Maybe I didn't yet find any time to add my work here or my work is always just a piece of shit. Who knows?</h1>
+    <h1>Maybe I'm too lazy to add my work here or my work is always just a piece of shit. Who knows?</h1>
     </div>
   ) 
 }
