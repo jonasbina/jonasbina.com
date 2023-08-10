baa7d397cdcbfe8f5c35616d83b58f1ea7d05a3c
Jonas <jonysbina@gmail.com>
2023-08-10 21:44:56 +0200

    needed a place to store an image to test some code XXXXX

--- pages/aboutme.js
+++ pages/aboutme.js
@@ -4,11 +4,8 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-    <h4>My name is Jonas Bina</h4>
-    <a>I live in the Czech republic</a>
-    <a>I like programming android apps, webs, games or plugins..</a>
-    <a>\                                                      /</a>
-    <a> ------------------------------------------------------</a>
+    <h4>I'm a 13(as of April 2023) years old begginner Android Developer. </h4>
+    
     </div>
   ) 
 }
