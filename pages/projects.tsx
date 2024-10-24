84c53627e36f359c9ed03e5f5201f2d6476ecc0f
Jonas <jonysbina@gmail.com>
2024-10-24 21:32:22 +0200

    pow(blyat,2)

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -3,7 +3,17 @@ import RootLayout from "./layout";
 export default function Home() {
     return(
         <RootLayout>
-            <h1>I'm too lazy to put things in here sorry :/</h1>
+            <div
+                className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] min-w-full">
+                <main className="flex flex-col gap-8 row-start-2 items-center text-center justify-center min-w-max">
+                    <div
+                        className="text-4xl text-center font-[family-name:var(--font-geist-mono)] animate__animated animate__slideInDown">
+                        <h1>I'm too lazy to put things in here for now sorry :/</h1>
+                    </div>
+                </main>
+            </div>
+
+
         </RootLayout>
-    )
+)
 }
