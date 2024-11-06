aac86345ee153b4309d4c03f1f9d5c1317e1bc82
GHB 3D Tisk <jonysbina@gmail.com>
2024-11-06 15:31:37 +0100

    tos for ghb so i cant get sued

---
+++ pages/ghb/tos.tsx
@@ -0,0 +1,190 @@
+import RootLayout from "../layout";
+
+export default function TOSGHB() {
+    return (
+        <RootLayout>
+            <div className="p-8">
+                <strong>Podmínky použití</strong><br/>
+                <p>Tyto podmínky použití se vztahují na aplikaci GHB (dále jen "Aplikace") pro mobilní zařízení, kterou
+                    vytvořil Jonáš Bína (dále jen "Poskytovatel služby") jako bezplatnou službu.</p><br/>
+                <p>Stažením nebo používáním Aplikace automaticky souhlasíte s následujícími podmínkami. Důrazně
+                    doporučujeme, abyste si tyto podmínky před použitím Aplikace důkladně přečetli a porozuměli jim.
+                    Jakékoli neoprávněné kopírování, modifikace Aplikace nebo její části, či našich ochranných známek je
+                    přísně zakázáno. Jakékoli pokusy o získání zdrojového kódu Aplikace, její překlad do jiných jazyků
+                    nebo vytváření odvozených verzí nejsou povoleny. Veškerá práva k ochranným známkám, autorská práva,
+                    práva k databázím a další práva k duševnímu vlastnictví související s Aplikací zůstávají majetkem
+                    Poskytovatele služby.</p><br/>
+                <p>Poskytovatel služby se zavazuje zajistit, aby byla Aplikace co nejprospěšnější a nejefektivnější.
+                    Proto si vyhrazuje právo kdykoli a z jakéhokoli důvodu Aplikaci upravit nebo zpoplatnit své služby.
+                    Poskytovatel služby vás ujišťuje, že jakékoliv poplatky za Aplikaci nebo její služby vám budou jasně
+                    oznámeny.</p><br/>
+                <p>Aplikace ukládá a zpracovává osobní údaje, které jste poskytli Poskytovateli služby za účelem
+                    poskytování Služby. Je vaší povinností udržovat bezpečnost vašeho telefonu a přístup k Aplikaci.
+                    Poskytovatel služby důrazně nedoporučuje "jailbreak" nebo "rootování" vašeho telefonu, což znamená
+                    odstranění softwarových omezení a limitů uložených oficiálním operačním systémem vašeho zařízení.
+                    Takové postupy mohou vystavit váš telefon škodlivému softwaru, virům, zákeřným programům, narušit
+                    bezpečnostní funkce telefonu a mohou vést k tomu, že Aplikace nebude správně fungovat nebo přestane
+                    fungovat zcela.</p>
+                <div>
+                    <p>Upozorňujeme, že Aplikace využívá služby třetích stran, které mají vlastní podmínky použití. Níže
+                        jsou odkazy na podmínky třetích stran používané Aplikací:</p>
+                    <ul>
+                        <li><a href="https://policies.google.com/terms" target="_blank" rel="noopener noreferrer">Google
+                            Play Services</a></li>
+                        <li><a href="https://www.google.com/analytics/terms/" target="_blank" rel="noopener noreferrer">Google
+                            Analytics for Firebase</a></li>
+                        <li><a href="https://firebase.google.com/terms/crashlytics" target="_blank"
+                               rel="noopener noreferrer">Firebase Crashlytics</a></li>
+                    </ul>
+                </div>
+                <br/>
+                <p>Prosím vezměte na vědomí, že Poskytovatel služby nenese odpovědnost za některé aspekty. Některé
+                    funkce Aplikace vyžadují aktivní připojení k internetu, které může být zajištěno přes Wi-Fi nebo
+                    mobilního operátora. Poskytovatel služby nenese odpovědnost, pokud Aplikace nefunguje na plný výkon
+                    z důvodu nedostupnosti Wi-Fi nebo vyčerpání vašeho datového limitu.</p><br/>
+                <p>Pokud používáte aplikaci mimo Wi-Fi síť, mějte na paměti, že se stále vztahují podmínky vaší dohody s
+                    mobilním operátorem. To může znamenat, že vám mohou vzniknout poplatky za datové přenosy nebo jiné
+                    poplatky třetích stran během připojení k aplikaci. Používáním aplikace přijímáte odpovědnost za tyto
+                    poplatky, včetně poplatků za roaming, pokud používáte aplikaci mimo svou domovskou oblast (tj.
+                    region nebo zemi) bez vypnutí datového roamingu. Pokud nejste plátcem za zařízení, na kterém
+                    aplikaci používáte, předpokládá se, že máte svolení od plátce.</p><br/>
+                <p>Stejně tak Poskytovatel služby nemůže vždy nést odpovědnost za vaše používání aplikace. Například je
+                    vaší odpovědností zajistit, aby bylo vaše zařízení nabité. Pokud se vám vybije baterie a nebudete
+                    mít přístup ke Službě, Poskytovatel služby za to nemůže být odpovědný.</p><br/>
+                <p>Co se týče odpovědnosti Poskytovatele služby za vaše používání aplikace, je důležité si uvědomit, že
+                    i když se snaží zajistit, aby byla aktuální a přesná, spoléhá se na třetí strany, které mu poskytují
+                    informace k zpřístupnění. Poskytovatel služby nepřijímá odpovědnost za žádnou ztrátu, přímou nebo
+                    nepřímou, kterou zažijete jako výsledek spoléhání se výhradně na tuto funkčnost aplikace.</p><br/>
+                <p>Poskytovatel služby může chtít v budoucnu aplikaci aktualizovat. Aplikace je momentálně dostupná
+                    podle požadavků pro operační systém (a případné další systémy, na které se rozhodne rozšířit
+                    dostupnost aplikace) mohou změnit, a vy budete muset stáhnout aktualizace, pokud budete chtít
+                    aplikaci nadále používat. Poskytovatel služby nezaručuje, že bude aplikaci vždy aktualizovat tak,
+                    aby byla relevantní a/nebo kompatibilní s konkrétní verzí operačního systému nainstalovaného na
+                    vašem zařízení. Nicméně souhlasíte s tím, že vždy přijmete aktualizace aplikace, když vám budou
+                    nabídnuty. Poskytovatel služby může také přestat poskytovat aplikaci a může ukončit její používání
+                    kdykoli bez předchozího oznámení. Pokud vás o tom jinak neinformuje, při ukončení platnosti (a)
+                    veškerá práva a licence vám udělená v těchto podmínkách zaniknou; (b) musíte přestat používat
+                    aplikaci a (v případě potřeby) ji odstranit ze svého zařízení.</p><br/>
+                <strong>Změny v těchto Podmínkách</strong>
+                <p>Poskytovatel služby může čas od času aktualizovat své Podmínky použití. Proto se doporučuje, abyste
+                    tuto stránku pravidelně kontrolovali kvůli změnám. Poskytovatel služby vás o všech změnách bude
+                    informovat zveřejněním nových Podmínek použití na této stránce.</p><br/>
+                <p>Tyto podmínky použití jsou účinné od 6. 11. 2024</p><br/>
+                <strong>Kontaktujte nás</strong>
+                <p>Máte-li jakékoli dotazy nebo připomínky k Podmínkám použití, neváhejte kontaktovat Poskytovatele
+                    služby na jonasbina@proton.me.</p>
+            </div>
+            <br/>
+            <div className="p-8">
+                <strong>Terms &amp; Conditions</strong><br/><p>These terms and conditions applies to the GHB app (hereby
+                referred
+                to
+                as "Application") for mobile devices that was created by Jonáš Bína (hereby referred to as "Service
+                Provider")
+                as a Free service.</p><br/><p>Upon downloading or utilizing the Application, you are automatically
+                agreeing
+                to
+                the following terms. It is strongly advised that you thoroughly read and understand these terms prior to
+                using
+                the Application. Unauthorized copying, modification of the Application, any part of the Application, or
+                our
+                trademarks is strictly prohibited. Any attempts to extract the source code of the Application, translate
+                the
+                Application into other languages, or create derivative versions are not permitted. All trademarks,
+                copyrights,
+                database rights, and other intellectual property rights related to the Application remain the property
+                of
+                the
+                Service Provider.</p><br/><p>The Service Provider is dedicated to ensuring that the Application is as
+                beneficial
+                and efficient as possible. As such, they reserve the right to modify the Application or charge for their
+                services at any time and for any reason. The Service Provider assures you that any charges for the
+                Application
+                or its services will be clearly communicated to you.</p><br/><p>The Application stores and processes
+                personal
+                data that you have provided to the Service Provider in order to provide the Service. It is your
+                responsibility
+                to maintain the security of your phone and access to the Application. The Service Provider strongly
+                advise
+                against jailbreaking or rooting your phone, which involves removing software restrictions and
+                limitations
+                imposed by the official operating system of your device. Such actions could expose your phone to
+                malware,
+                viruses, malicious programs, compromise your phone's security features, and may result in the
+                Application
+                not
+                functioning correctly or at all.</p>
+                <div><p>Please note that the Application utilizes third-party services that have their own Terms and
+                    Conditions.
+                    Below are the links to the Terms and Conditions of the third-party service providers used by the
+                    Application:</p>
+                    <ul>
+                        <li><a href="https://policies.google.com/terms" target="_blank" rel="noopener noreferrer">Google
+                            Play
+                            Services</a></li>
+                        <li><a href="https://www.google.com/analytics/terms/" target="_blank" rel="noopener noreferrer">Google
+                            Analytics for Firebase</a></li>
+                        <li><a href="https://firebase.google.com/terms/crashlytics" target="_blank"
+                               rel="noopener noreferrer">Firebase
+                            Crashlytics</a></li>
+                    </ul>
+                </div>
+                <br/><p>Please be aware that the Service Provider does not assume responsibility for certain aspects.
+                Some
+                functions of the Application require an active internet connection, which can be Wi-Fi or provided by
+                your
+                mobile network provider. The Service Provider cannot be held responsible if the Application does not
+                function at full capacity due to lack of access to Wi-Fi or if you have exhausted your data
+                allowance.</p>
+                <br/><p>If you are using the application outside of a Wi-Fi area, please be aware that your mobile
+                network
+                provider's agreement terms still apply. Consequently, you may incur charges from your mobile
+                provider
+                for data usage during the connection to the application, or other third-party charges. By using the
+                application, you accept responsibility for any such charges, including roaming data charges if you
+                use
+                the application outside of your home territory (i.e., region or country) without disabling data
+                roaming.
+                If you are not the bill payer for the device on which you are using the application, they assume
+                that
+                you have obtained permission from the bill payer.</p><br/><p>Similarly, the Service Provider cannot
+                always assume responsibility for your usage of the application. For instance, it is your
+                responsibility
+                to ensure that your device remains charged. If your device runs out of battery and you are unable to
+                access the Service, the Service Provider cannot be held responsible.</p><br/><p>In terms of the
+                Service
+                Provider's responsibility for your use of the application, it is important to note that while they
+                strive to ensure that it is updated and accurate at all times, they do rely on third parties to
+                provide
+                information to them so that they can make it available to you. The Service Provider accepts no
+                liability
+                for any loss, direct or indirect, that you experience as a result of relying entirely on this
+                functionality of the application.</p><br/><p>The Service Provider may wish to update the application
+                at
+                some point. The application is currently available as per the requirements for the operating system
+                (and
+                for any additional systems they decide to extend the availability of the application to) may change,
+                and
+                you will need to download the updates if you want to continue using the application. The Service
+                Provider does not guarantee that it will always update the application so that it is relevant to you
+                and/or compatible with the particular operating system version installed on your device. However,
+                you
+                agree to always accept updates to the application when offered to you. The Service Provider may also
+                wish to cease providing the application and may terminate its use at any time without providing
+                termination notice to you. Unless they inform you otherwise, upon any termination, (a) the rights
+                and
+                licenses granted to you in these terms will end; (b) you must cease using the application, and (if
+                necessary) delete it from your device.</p><br/><strong>Changes to These Terms and Conditions</strong>
+                <p>The Service Provider may periodically update their Terms and Conditions. Therefore, you are
+                    advised
+                    to review this page regularly for any changes. The Service Provider will notify you of any
+                    changes
+                    by posting the new Terms and Conditions on this page.</p><br/><p>These terms and conditions are
+                effective as of 2024-11-06</p><br/><strong>Contact Us</strong><p>If you have any questions or
+                suggestions about the Terms and Conditions, please do not hesitate to contact the Service
+                Provider
+                at jonasbina@proton.me.</p>
+            </div>
+        </RootLayout>
+    )
+}
